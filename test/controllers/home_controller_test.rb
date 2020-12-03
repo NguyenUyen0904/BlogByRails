d15eed00e34058698a295ffb2333f6a53682a856
NguyenUyen0904 <nguyen.uyen.0904@gmail.com>
2020-12-03 02:50:22 +0000

    transition to post

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
+   test "should get home" do
+    get home_home_url
+    assert_response :success
+  end
   #   assert true
   # end
 end
